0a90b18e67812a641a1fd08e7f85feda968e2f58
Iaannn06 <alfianazad05@gmail.com>
2024-05-31 11:18:47 +0700

    membuat  menu

--- CircularQueuesArray_0126/CircularQueuesArray_0126.cpp
+++ CircularQueuesArray_0126/CircularQueuesArray_0126.cpp
@@ -1,5 +1,5 @@
-using namespace std;
 #include <iostream>
+using namespace std;
 
 class Queues {
 	int FRONT, REAR, max = 5;
@@ -13,12 +13,12 @@ public:
 
 	void insert() {
 		int num;
-		cout << "Enter a number: ";
+		cout << "enter a number: ";
 		cin >> num;
 		cout << endl;
 
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-			cout << "\nQueues overflow\n";
+			cout << "\nQueue overflow\n";
 			return;
 		}
 
@@ -27,13 +27,14 @@ public:
 			REAR = 0;
 		}
 		else {
-			if (REAR == max -1)
+			if (REAR == max - 1)
 				REAR = 0;
 			else
 				REAR = REAR + 1;
+
 		}
 		queue_array[REAR] = num;
-    }
+	}
 
 	void remove() {
 		if (FRONT == -1) {
@@ -41,17 +42,14 @@ public:
 			return;
 		}
 		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
-
 		if (FRONT == REAR) {
 			FRONT = -1;
 			REAR = -1;
 		}
-
 		else {
 			if (FRONT == max - 1)
 				FRONT = 0;
-			else
-				FRONT = FRONT + 1;
+			else FRONT = FRONT + 1;
 		}
 	}
 
@@ -63,16 +61,76 @@ public:
 			cout << "Queue is empty\n";
 			return;
 		}
-
-		cout << "\nElements in the queue are...\n";
+		cout << "\nElements in the queue are....\n";
 
 		if (FRONT_position <= REAR_position) {
 			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "    ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else
+		{
+			while (FRONT_position <= max - 1)
+			{
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			FRONT_position = 0;
+
+			while (FRONT_position <= REAR_position)
+			{
 				cout << queue_array[FRONT_position] << "  ";
 				FRONT_position++;
 			}
 			cout << endl;
-
 		}
 	}
+
 };
+
+int main() {
+	Queues q;
+	char ch;
+
+	while (true)
+	{
+		try {
+			cout << "menu" << endl;
+			cout << "1. impelement insert operation" << endl;
+			cout << "2. impelement delete operation" << endl;
+			cout << "3. display values" << endl;
+			cout << "4. exit" << endl;
+			cout << "enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case'1': {
+				q.insert();
+				break;
+			}
+			case'2': {
+				q.remove();
+				break;
+			}
+			case'3': {
+				q.display();
+				break;
+			}
+			case'4': {
+				return 0;
+			}
+			default: {
+				cout << "invalid option!!" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "check for the value entered." << endl;
+		}
+	}
+	return 0;
+}
